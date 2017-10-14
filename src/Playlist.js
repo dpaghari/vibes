be9b8c11ee1261da0ce13e05861366b8dc2b0684
Daniel Pagharion <dspagharion@gmail.com>
2017-10-13 19:44:42 -0700

    First commit

--- src/Playlist.js
+++ src/Playlist.js
@@ -1,3 +1,5 @@
+import SpotifyWebApi from 'spotify-web-api-js';
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
@@ -9,8 +11,24 @@ export default class Playlist extends React.Component {
     this.filterList = this.filterList.bind(this);
     this.state = {
       songs: props.songs,
-      addingSong: false
+      addingSong: false,
+      spotify: {}
     };
+
+  }
+
+  componentDidMount() {
+    var spotifyApi = new SpotifyWebApi();
+    spotifyApi.setAccessToken('BQBgZjupvnVmQfgOD9l3c2wg_kg8R523quEOsnp_p1LPhiZlzxIQEG-6ma92QBrztNXYv5ifmyIj4pc6dc0azga9NuI4GTFwz77_fBP-_r7PKhQ5-GGBNwcCrIi7uQAIkt5Xj9UOQFLdBGk6OIu_61sZBBs-p271PJziXwZgrlwHP5dfgCpl0EnCh0ZmDetCbw39cpuPqaDoFBOfbPkM5ElRxAK_p_vzm5hwqPFIRd2gkpyJQNW5lzkUxxBKAXOkHDzli2KI0uO-nt17cY0beo3viDBvTWz6');
+    spotifyApi.getPlaylist('dpaghari', '6JMfX4X1l9yKOYa1zFnDqf')
+    .then(function(data) {
+    console.log('User playlists', data);
+    this.setState({
+      songs: data.tracks.items
+    });
+  }.bind(this), function(err) {
+      console.error(err);
+    });
   }
 
   filterList(e) {
@@ -24,6 +42,7 @@ export default class Playlist extends React.Component {
   }
 
   render() {
+
     return (
       <div className="playList">
         <div className="playList__img-wrapper">
@@ -79,17 +98,24 @@ export default class Playlist extends React.Component {
     renderSongs() {
     let songs = this.state.songs;
     return songs.map((song, idx) => {
-        const { songName, albumName, artistName, albumImg } = song;
-        return (
-          <li key={ idx }>
+        // const { songName, albumName, artistName, albumImg } = song;
+        console.log(song);
+        if(song.track) {
+          var songName = song.track.name;
+          var artistName = song.track.artists[0].name;
+          var albumName = song.track.album.name;
+          var albumImg = song.track.album.images[0].url
+          return (
+            <li key={ idx }>
             <img src={albumImg} alt={albumImg}/>
             <div className="songInfo">
-              <h3>{songName}</h3>
-              <h5>{artistName}</h5>
-              <h6>{albumName}</h6>
+            <h3>{songName}</h3>
+            <h5>{artistName}</h5>
+            <h6>{albumName}</h6>
             </div>
-          </li>
-        );
+            </li>
+          );
+        }
     });
   }
 }
